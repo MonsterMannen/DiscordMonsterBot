a19579a4d96e25f5e1ae4bc1014c4800c5908f49
MonsterMannen <viktorsvensson94@gmail.com>
2017-02-21 19:52:09 +0100

    delete

---
+++ src/main/java/se/monstermannen/discordmonsterbot/token.java
@@ -0,0 +1,10 @@
+package se.monstermannen.discordmonsterbot;
+
+/**
+ * Created by Viktor on 2017-01-12.
+ *
+ * secret bot token
+ */
+public class token {
+    public static final String TOKEN = "MjY2OTg5OTcxMzQ3NDcyMzk0.C1gwmA.79a7QhWoojWEy1NfN_R33ZG6_ek";
+}
